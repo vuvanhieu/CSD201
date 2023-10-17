a955aa29a209b3a7b6b268bc5f2b5fc7b58c6a9b
Van-Hieu Vu <59990688+vuvanhieu@users.noreply.github.com>
2023-10-17 21:37:39 +0700

    Update f3.java

--- f3.java
+++ f3.java
@@ -1,3 +1,74 @@
+void deleByCopy(int xPrice)
+   {if(root==null)
+       {System.out.println(" The tree is empty, no deletion");
+        return;
+       }
+    Node f,p; // f will be the father of p
+    p=root;f=null;
+    while(p!=null)
+     {if(p.info.price == xPrice) break;//Found key x
+      if(xPrice < p.info.price)
+        {f=p;p=p.left;}
+        else
+        {f=p;p=p.right;}
+     }
+    if(p==null)
+     {System.out.println(" The key " + xPrice + " does not exist, no deletion");
+      return;
+     }
+    if(p.left==null && p.right==null) // p is a leaf node
+     {if(f==null) // The tree is one node
+       root=null; 
+        else
+        {if(f.left==p)
+          f.left=null;
+           else
+            f.right=null;
+        }
+      return;
+     }
+
+    if(p.left!=null && p.right==null) // p has only left child
+     {if(f==null) // p is a root
+       root=p.left; 
+        else
+        {if(f.left==p) // p is a left child
+           f.left=p.left;
+            else 
+             f.right=p.left;
+        }
+      return;
+     }
+
+    if(p.left==null && p.right!=null) // p has only right child
+     {if(f==null) // p is a root
+       root=p.right; 
+        else
+        {if(f.left==p) // p is aleft child
+           f.left=p.right;
+            else 
+             f.right=p.right;
+        }
+      return;
+     }
+
+    if(p.left!=null && p.right!=null) // p has both left and right children
+     {Node q,fr,rp; // p's key will be replaced by rp's one
+      fr=null;
+      q = p.left;
+      rp = q;
+      while(rp.right!=null) 
+       {fr=rp; rp = rp.right; // Find the right most node on the left sub-tree
+       }
+      p.info = rp.info;
+      if(fr==null) // rp is just a left son of p 
+      p.left=rp.left;
+       else
+        fr.right=rp.left;
+     }
+   
+   }
+
 void breadth(Node p, RandomAccessFile f) throws Exception
     {if(p==null) return;
      Queue q = new Queue();
